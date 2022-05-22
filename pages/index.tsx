ef73ffcefea12aad1457d27634e4ceddf9d1f793
Graham Swan <thinkswan@gmail.com>
2022-05-21 20:57:31 -0500

    fix: use HTML entity for apostrophe

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,8 +36,8 @@ export default function Home({
           Hello, I&apos;m <strong>Graham</strong>.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you&apos;ll be building a site like this
+          on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
 
